a7ce7d893f2104cb1cb2edb7f9126c105310881f
Ulvy156 <ulvyromy156@gmail.com>
2025-07-04 14:19:43 +0700

    feat: add various icons and layouts, implement internationalization, and set up main application structure
    - Added new icon components: IconDocumentation, IconEcosystem, IconSetting, IconSupport, IconTooling.
    - Created HR-Layout and Staff-Layout with basic structure and logo.
    - Initialized English and Khmer locale files for internationalization.
    - Set up main application entry point in main.ts.
    - Implemented Axios plugin for API requests.
    - Configured Vue Router with basic routes for Home and About views.
    - Established Pinia store for counter functionality.
    - Developed utility functions for dark mode, loading state, and notifications.
    - Created AboutView and HomeView components with basic content.
    - Configured TypeScript settings for the application.
    - Set up Vite configuration with plugins for Vue, Tailwind CSS, and auto-importing components.

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <StaffLayout></StaffLayout>
+</template>
+
+<script setup lang="ts">
+import StaffLayout from './layouts/Staff-Layout.vue';
+</script>
---
+++ src/layouts/Staff-Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="flex">
+    <aside class="h-[100vh] w-[20%] shadow-md">
+      <div>
+        <img src="../assets/logo.svg" alt="" />
+      </div>
+    </aside>
+    <header class=" w-full h-fit flex justify-between">
+      <h1>Welcome back Ulvy</h1>
+      <div class="flex">
+        <IconSetting/>
+      </div>
+    </header>
+  </section>
+</template>
+
+<script setup lang="ts">
+import IconSetting from '@/components/icons/IconSetting.vue';
+
+</script>
