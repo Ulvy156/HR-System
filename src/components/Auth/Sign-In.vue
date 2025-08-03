d1f34c613bd5197b363ee214033aa1f382214e97
Ulvy Romy <ulvyromy156@gmail.com>
2025-08-03 18:03:59 +0700

    feat: enhance Sign-In component with validation and dynamic input status

--- src/components/Auth/Sign-In.vue
+++ src/components/Auth/Sign-In.vue
@@ -1,17 +1,18 @@
 <template>
   <section class="grid grid-cols-1 gap-5 ">
     <div class="grid grid-cols-1">
-      <p>Email:</p>
+      <p :class="inputStatus.email">Email:</p>
       <BaseInput
+        :status="inputStatus.email"
         placeholder="Enter your email"
         type="email"
-        status="default"
         v-model="formData.email"
       />
     </div>
     <div class="grid grid-cols-1 ">
-      <p>Password:</p>
+      <p :class="inputStatus.password">Password:</p>
       <BaseInput
+        :status="inputStatus.password"
         v-model="formData.password"
         placeholder="Enter your password"
         show-password-on="click"
@@ -25,7 +26,7 @@
       :ghost="true">
         Sign In
       </BaseButton>
-      <a href="">Forgot password?</a>
+      <span class="underline text-blue-800 cursor-pointer">forgot password?</span>
     </div>
   </section>
 </template>
@@ -36,19 +37,30 @@ import authAPI from '@/services/auth-service';
 //utils
 
 //component
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import BaseInput from '../base/BaseInput.vue';
 import BaseButton from '../base/BaseButton.vue';
 import { setCookie } from '@/utils/useCookies';
+import type { InputStatus } from '@/types/InputType';
 
 //properties
 const formData = ref({
   email: '',
   password: ''
 })
-
+const validateForm = ref({
+  is_email_invalid: false,
+  is_password_invalid: false
+});
 //methods
 async function handleSignIn() {
+  // Validate form data before sending
+  checkValidation();
+  if (!isValidData.value) {
+    console.error('Invalid data');
+    return;
+  }
+  // Send sign-in request
   await authAPI.post('/auth/login', formData.value)
     .then((response) => {
       const access_token = response.data.access_token;
@@ -63,4 +75,38 @@ async function handleSignIn() {
       // Handle sign-in failure (e.g., show error message)
     });
 }
+function checkValidation() {
+  if(formData.value.password.trim().length < 8) {
+    validateForm.value.is_password_invalid = true;
+  }
+  else {
+    validateForm.value.is_password_invalid = false;
+  }
+  if (formData.value.email.trim().length === 0) {
+    validateForm.value.is_email_invalid = true;
+  }
+  else {
+    validateForm.value.is_email_invalid = false;
+  }
+}
+
+//computed
+const inputStatus = computed(() => {
+  return {
+    email: validateForm.value.is_email_invalid ? 'error' : 'default' as InputStatus,
+    password: validateForm.value.is_password_invalid ? 'error' : 'default' as InputStatus
+  };
+});
+const isValidData = computed(() => {
+  return validateForm.value.is_email_invalid && validateForm.value.is_password_invalid;
+});
 </script>
+
+<style scoped>
+.default {
+  color: black;
+}
+.error {
+  color: red;
+}
+</style>
